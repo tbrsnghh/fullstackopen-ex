4748f7133674fb99aeb5c9d0bf84b986f878c1c0
tbrsnghh <nhham1771@gmail.com>
2026-03-31 06:52:33 +0700

    4.15

--- part4/bloglist/app.js
+++ part4/bloglist/app.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const mongoose = require('mongoose')
 const blogsRouter = require('./controllers/blogs')
+const usersRouter = require('./controllers/users')
 const middleware = require('./utils/middleware')
 const config = require('./utils/config')
 const logger = require('./utils/logger')
@@ -11,6 +12,7 @@ logger.info('connected to MongoDB')
 
 app.use(express.json())
 app.use('/api/blogs', blogsRouter)
+app.use('/api/users', usersRouter)
 app.use(middleware.unknownEndpoint)
 app.use(middleware.errorHandler)
 
